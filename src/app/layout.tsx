82252ddb27e154766443b5fdacf008fbe662ecb8
chedi <dayehchadi56@gmail.com>
2026-01-08 08:22:44 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "KESHAH Analytics",
+  title: "Facture app",
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
